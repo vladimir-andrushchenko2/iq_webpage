ad2464e9562fe2eda3de1e50ae2e1d25dc527dbb
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-18 22:21:33 +0300

    added button and ready to work on styles

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true
+  },
+  current: {
+    type: Number,
+    required: true
+  }
+})
+
+const progress = (props.current / props.total) * 100
+</script>
+
+<template>
+  <div class="progress-bar">
+    <div class="indicator" :style="{ width: progress + '%' }"></div>
+  </div>
+</template>
+
+<style scoped>
+.progress-bar {
+  height: 11px;
+  width: 100%;
+  background-color: #f2f3f3aa;
+  border-radius: 10.5px;
+}
+
+.indicator {
+  height: 100%;
+  background-color: #3bde7c;
+
+  border-radius: 10.5px;
+}
+</style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BaseHeader from '@/components/BaseHeader.vue'
+import ProgressBar from '@/components/ProgressBar.vue'
 import { useOverflowHiddenOnBody } from '@/composables/useOverflowHiddenOnBody'
 
 useOverflowHiddenOnBody()
@@ -11,7 +12,13 @@ useOverflowHiddenOnBody()
   <div class="page">
     <BaseHeader></BaseHeader>
     <main class="main">
-      <div class="progress-bar"></div>
+      <div class="progress-bar-wrapper">
+        <ProgressBar :total="13" :current="4" />
+      </div>
+
+      <div class="question" style="color: white">Lol</div>
+
+      <button class="continue-btn" to="/test">Далее</button>
     </main>
   </div>
 </template>
@@ -28,5 +35,27 @@ useOverflowHiddenOnBody()
   display: flex;
   flex-direction: column;
   flex-grow: 1;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.progress-bar-wrapper {
+  margin: 17px auto 0;
+  width: 260px;
+}
+
+.continue-btn {
+  display: block;
+  padding: 14px 17px 11px;
+  border-radius: 20px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  color: black;
+  min-width: 189px;
+  background-color: #ffc700;
+  margin-bottom: 25px;
 }
 </style>
