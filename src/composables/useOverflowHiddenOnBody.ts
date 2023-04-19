cac87547336176493b4906201e4f07465d20d2d1
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-19 12:48:13 +0300

    moved fix to the respected component

--- src/composables/useOverflowHiddenOnBody.ts
+++ src/composables/useOverflowHiddenOnBody.ts
@@ -3,6 +3,7 @@ import { onMounted, onUnmounted } from 'vue'
 export function useOverflowHiddenOnBody() {
   onMounted(() => {
     document.body.style.overflow = 'hidden'
+    window.scrollTo({ top: 0 })
   })
 
   onUnmounted(() => {
